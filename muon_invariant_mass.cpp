d43d7f4334c97fda89ed5315e341c9bcc04e607d
kayamash <yamashita@stu.kobe-u.ac.jp>
2018-07-28 03:33:37 +0900

    value

--- muon_invariant_mass.cpp
+++ muon_invariant_mass.cpp
@@ -118,7 +118,7 @@ void muon_invariant_mass(){
   latex.SetTextSize(0.03);
   latex.SetNDC(1);
   //experiment data
-  string label_experiment = "Experimental_value"；
+  //string label_experiment = "Experimental_value"；
   string label_JP = Form("J/#psi = %d #pm ",static_cast<int>(mean_JP));  
   label_JP += Form("%d [MeV]",static_cast<int>(err_JP));
   string label_Y1S = Form("Y1S = %d #pm ",static_cast<int>(mean_Y1S));  
@@ -137,7 +137,7 @@ void muon_invariant_mass(){
   label_v_Y2S += "0.31 [MeV]";
   string label_v_Z = "Z = 91187.6 #pm ";  
   label_v_Z += "2.1 [MeV]";
-  latex.DrawLatex(0.4,0.9,label_experiment.c_str());
+  //latex.DrawLatex(0.4,0.9,label_experiment.c_str());
   latex.DrawLatex(0.4,0.8,label_JP.c_str());
   latex.DrawLatex(0.4,0.7,label_Y1S.c_str());
   latex.DrawLatex(0.4,0.6,label_Y2S.c_str());
