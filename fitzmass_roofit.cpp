2d3d174d9a75b04beb4fa1572138ec457393143e
kayamash <yamashita@stu.kobe-u.ac.jp>
2018-08-01 09:35:35 +0900

    a

--- fitzmass_roofit.cpp
+++ fitzmass_roofit.cpp
@@ -1,34 +1,31 @@
 using namespace RooFit;
  
 void fitzmass_roofit(){
-  std::cout<<"------------"<<endl;
   TTree *tree = new TTree("tree","tree");
   int nevt = tree->ReadFile("Data.txt","x");
-  std::cout<<"----------------"<<endl; 
   RooWorkspace w("w");
-  w.factory("x[60000,120000]");  // invariant mass
-  std::cout<<"-------------------"<<endl;
-  //w.factory("nbkg[120000, 0, 120000]");
-  //w.var("nbkg")->setVal(nevt);
-  //w.var("nbkg")->setMin(0.1*nevt);
-  //w.var("nbkg")->setMax(10*nevt);
+  w.factory("x[60000,120000]");  // invariant mass draw range
+  w.factory("nbkg[30000, 0, 50000]"); // the number of background
+  w.var("nbkg")->setVal(nevt);
+  w.var("nbkg")->setMin(0.1*nevt);
+  w.var("nbkg")->setMax(10*nevt);
   
-  // create exponential model as two components
-  //w.factory("a1[ 7.5, -5000, 5000]");
-  //w.factory("a2[-1.5, -5000, 5000]");
-  //w.factory("expr::z('-(a1*x/100 + a2*(x/100)^2)', a1, a2, x)");
-  //w.factory("Exponential::model_bkg(z, 1)");
+  //create exponential model as two components
+  w.factory("a1[ 7.5, -5000, 5000]");
+  w.factory("a2[-1.5, -5000, 5000]");
+  w.factory("expr::z('-(a1*x + a2*x^2)', a1, a2, x)");
+  w.factory("Exponential::model_bkg(z, 1)");
  
   // signal model  
-  //w.factory("nsig[100000, 0., 100000.0]");
-  w.factory("mass[90000, 60000, 120000]");
-  w.factory("width[1, 0.5,100]");
+  w.factory("nsig[5000, 0., 10000.0]");// the number of signal
+  w.factory("mass[90000, 60000, 120000]");// mean?
+  w.factory("width[1, 0.5,10000]");//sigma?
   w.factory("Gaussian::model_sig(x, mass, width)");
  
   RooAbsPdf * model_sig = w.pdf("model_sig");
  
-  //w.factory("SUM::model(nbkg*model_bkg, nsig*model_sig)");
-  RooAbsPdf * model = w.pdf("model");
+  w.factory("SUM::model(nbkg*model_bkg, nsig*model_sig)");
+  RooAbsPdf * model = w.pdf("model");//the pdf model of signal and background
  
   // create RooDataSet
   RooDataSet data("data","data", tree, *w.var("x"));
@@ -39,7 +36,7 @@ void fitzmass_roofit(){
   RooPlot * plot = w.var("x")->frame();
   data.plotOn(plot);
   model->plotOn(plot);
- // model->plotOn(plot, Components("model_bkg"),LineStyle(kDashed));
+  model->plotOn(plot, Components("model_bkg"),LineStyle(kDashed));
   model->plotOn(plot, Components("model_sig"),LineColor(kRed));
   plot->Draw();
 }
